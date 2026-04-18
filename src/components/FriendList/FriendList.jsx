b22b90dcf1ac1e284219c6f44939c7daa606fb68
Nick Rashchuk <raschyk1993@gmail.com>
2026-04-18 23:15:29 +0300

    third task

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,17 @@
+import styles from './FriendList.module.css';
+import { FriendListItem } from './FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={styles.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          name={name}
+          avatar={avatar}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,17 @@
+import styles from './FriendList.module.css';
+import clsx from 'clsx';
+
+export const FriendListItem = ({ name, avatar, isOnline, key }) => {
+  return (
+    <li className={styles.item} key={key}>
+      <span
+        className={clsx(
+          styles.status,
+          isOnline ? styles.online : styles.offline
+        )}
+      ></span>
+      <img className={styles.avatar} src={avatar} alt={name} width="48" />
+      <p className={styles.name}>{name}</p>
+    </li>
+  );
+};
